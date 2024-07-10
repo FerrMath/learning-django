b6fddf9062f3540be169aeb22bdb6a866816e9a6
Matheus A <maf_dev@hotmail.com>
2024-07-10 01:14:54 -0300

    Finish display data to front end from models

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Recipe
 
 # Create your views here.
 def home_view(request):
+    recipes = Recipe.objects.all()
     content = {
         "title": "Recipes",
-        "content": 'Welcome to the home page of the Recipes'
+        "content": 'Welcome to the home page of the Recipes',
+        'recipes': recipes,
     }
     return render(request, 'recipes/pages/home.html', context=content) # Add the namespace to prevent conflicts with other templates
     
 def recipe_view(request, id):
+
     context = {
         "id": id,
-        "is_detail_page": True
+        "is_detail_page": True,
+        "recipe": get_object_or_404(Recipe, id=id)
     }
     return render (request, 'recipes/pages/recipe.html', context=context)
